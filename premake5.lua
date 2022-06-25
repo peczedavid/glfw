6e5db91fe4a24e37ca353f641a547138898ef67a
peczedavid <peczedavid@gmail.com>
2022-06-25 18:33:02 +0200

    premake update

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,7 @@
 project "GLFW"
 	kind "StaticLib"
 	language "C"
-	staticruntime "off"
+	staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -26,30 +26,6 @@ project "GLFW"
 		"src/window.c",
 	}
 
-	filter "system:linux"
-		pic "On"
-
-		systemversion "latest"
-		
-		files
-		{
-			"src/x11_init.c",
-			"src/x11_monitor.c",
-			"src/x11_window.c",
-			"src/xkb_unicode.c",
-			"src/posix_time.c",
-			"src/posix_thread.c",
-			"src/glx_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c",
-			"src/linux_joystick.c"
-		}
-
-		defines
-		{
-			"_GLFW_X11"
-		}
-
 	filter "system:windows"
 		systemversion "latest"
 
@@ -73,10 +49,12 @@ project "GLFW"
 			"_CRT_SECURE_NO_WARNINGS"
 		}
 
+		--[[
 		links
 		{
 			"Dwmapi.lib"
 		}
+		]]--
 
 	filter "configurations:Debug"
 		runtime "Debug"
@@ -85,8 +63,3 @@ project "GLFW"
 	filter "configurations:Release"
 		runtime "Release"
 		optimize "on"
-
-	filter "configurations:Dist"
-		runtime "Release"
-		optimize "on"
-        symbols "off"
